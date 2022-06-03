5507194822925d2e36daeefc196c9b10a0f359f4
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2022-06-03 11:07:08 -0300

    .

--- js/localidade.js
+++ js/localidade.js
@@ -1,40 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    nome = document.querySelector('.pais')
-    email = document.querySelector('.cidade')
+    pais = document.querySelector('.pais')
+    cidade = document.querySelector('.cidade')
     enviar = document.querySelector('input[type="submit"]')
-
-    enviar.disabled = true;
-
-    function update() {
-
-        if (nome.value == '' || email.value == ''){
-            enviar.disabled = true;
-        } else {
-            enviar.disabled = false;
-        }
-
-    }
-
-    nome.addEventListener('input', function() {
-        update()
-    })
-
-    email.addEventListener('input', function() {
-        update()
-    })
 
     enviar.addEventListener('click', function(e){
 
+        if(pais.value!='' && cidade.value!=''){
+            window.location.href = 'main.html'
+        }
         e.preventDefault();
-        window.location.href = 'main.html'
     })
-
-    p = document.querySelector('.sub')
-    p.addEventListener('click', function(e){
-        e.preventDefault()
-        window.alert('Preencha todos os campos obrigatórios.')
-        
-    })
-
 })
