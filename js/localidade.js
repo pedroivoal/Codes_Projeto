10c42dd67854a98fe8ba48594fd3f391dd7d6c26
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2022-06-04 14:04:38 -0300

    .

--- js/localidade.js
+++ js/localidade.js
@@ -4,16 +4,32 @@ document.addEventListener('DOMContentLoaded', function() {
     cidade = document.querySelector('.cidade')
     enviar = document.querySelector('input[type="submit"]')
 
+
+
     enviar.addEventListener('click', function(e){
 
         if(pais.value!='' && cidade.value!=''){
             window.location.href = 'main.html'
         }else{
             label = document.querySelectorAll('label')
-            label[0].style.color = 'red'
-            label[1].style.color = 'red'
-            
+            janela = document.querySelectorAll('.janela')
+            aparece()
+            setTimeout(desaparece, 3000)
         }
         e.preventDefault();
     })
 })
+
+function aparece(){
+    label[0].classList.add('red')
+    label[1].classList.add('red')
+    janela[0].classList.add('tra0')
+    janela[1].classList.add('tra1')
+}
+
+function desaparece(){
+    label[0].classList.remove('red')
+    label[1].classList.remove('red')
+    janela[0].classList.remove('tra0')
+    janela[1].classList.remove('tra1')
+}
